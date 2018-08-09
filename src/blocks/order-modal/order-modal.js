d1fd9682371996ec83873064bfce910af7d4ff8b
des-yogi <nuevo-design@mail.ru>
2018-08-10 00:24:11 +0300

    order-modals update

--- src/blocks/order-modal/order-modal.js
+++ src/blocks/order-modal/order-modal.js
@@ -2,46 +2,51 @@
 
 $( document ).ready(function() {
 
-  var EP560 =
-  {
-    name: 'Инверсионный стол EP-560',
-    model: 'ep-560',
-    img: 'ep-560-1-mobile@1x.jpg',
-    price: 4000,
-    amount: null
-  }
-
-  var EP970 =
-  {
-    name: 'Инверсионный стол EP-970',
-    model: 'ep-970',
-    img: 'ep-970-1-mobile@1x.jpg',
-    price: 6000,
-    amount: null
-  }
-
-  var CONTOURL5 =
-  {
-    name: 'Инверсионный стол Contour L5',
-    model: 'contour-l5',
-    img: 'contour-l5-1-mobile@1x.jpg',
-    price: 8000,
-    amount: null
-  }
-
-  var EZUP =
-  {
-    name: 'EZ-Up Chin-Up System',
-    model: 'ez-up',
-    img: 'ez-up_chin-up-1-mobile@1x.jpg',
-    price: 2000,
-    amount: null
-  }
+  var PRODUCTS =
+    [
+      {
+        name: 'Инверсионный стол EP-560',
+        shortName: 'EP-560',
+        kind: 'Инверсионный стол',
+        model: 'ep-560',
+        img: 'ep-560-1-mobile@1x.jpg',
+        price: 4000,
+        amount: null
+      },
+      {
+        name: 'Инверсионный стол EP-970 Ltd',
+        shortName: 'EP-970 Ltd',
+        kind: 'Инверсионный стол',
+        model: 'ep-970',
+        img: 'ep-970-1-mobile@1x.jpg',
+        price: 6000,
+        amount: null
+      },
+      {
+        name: 'Инверсионный стол Contour L5',
+        shortName: 'Contour L5',
+        kind: 'Инверсионный стол',
+        model: 'contour-l5',
+        img: 'contour-l5-1-mobile@1x.jpg',
+        price: 8000,
+        amount: null
+      },
+      {
+        name: 'Инверсионные ботинки EZ-Up Chin-Up System',
+        shortName: 'EZ-Up Chin-Up System',
+        kind: 'Инверсионные ботинки',
+        model: 'ez-up',
+        img: 'ez-up_chin-up-1-mobile@1x.jpg',
+        price: 2000,
+        amount: null
+      }
+    ];
 
   var buyBtns = Array.from(document.querySelectorAll('.btn--buy'));
   var orderModal = document.getElementById('order-modal');
   var prodName = orderModal.querySelector('#prodName');
-  var prodModel = orderModal.querySelector('#model');
+  var prodShortName = orderModal.querySelector('#shortName');
+  var prodKind = orderModal.querySelector('#kind');
   var prodImage = orderModal.querySelector('#prodImg');
 
   // console.log(prodName.textContent);
@@ -54,18 +59,22 @@ $( document ).ready(function() {
     if (e.target.dataset.model) {
       var target = e.target.dataset.model;
       console.log(target);
-      //prodName.textContent = target[name];
-      //prodImage.src = 'img/' + target[img];
-      console.log(prodImage.src);
+
+      for (product of PRODUCTS) {
+        if (target === product.model) {
+          prodName.textContent = product.shortName;
+          prodKind.textContent = product.kind;
+          prodShortName.textContent = product.shortName;
+          prodImage.src = 'img/' + product.img;
+        }
+      }
+
     } return;
   };
 
 
   for (var btn of buyBtns) {
     btn.addEventListener('click', buyEventHandler);
-    //console.log(btn);
   }
-
-  // console.log(buyBtns);
 
 });
